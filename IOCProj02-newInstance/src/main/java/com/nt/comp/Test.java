3393e05e549099d7d2d5de2b72ff03ff9be6930e
Shahjada87 <71275176+Shahjada87@users.noreply.github.com>
2021-09-16 20:01:20 +0530

    Second Commit

---
+++ IOCProj02-newInstance/src/main/java/com/nt/comp/Test.java
@@ -0,0 +1,13 @@
+package com.nt.comp;
+
+public class Test {
+	private int a = 10;
+	private String b ="hello";
+	
+	
+	//Alt + shift +s , s
+	@Override
+	public String toString() {
+		return "Test [a=" + a + ", b=" + b + "]";
+	}
+}
